efa5a79f8dccd21f801753872cd40e3f9e7902d2
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-11-01 16:20:45 +0100

    Getting started with socket.io

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const app = require("./app");
+const http = require("http");
+const socketio = require("socket.io");
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 
-app.listen(port, () => {
+const io = socketio(server);
+
+io.on("connection", () => {
+  console.log("New web socket connection");
+});
+
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
