6ffc5950d2fe90ffbdd44108c1ffac5659d3d5ce
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-11-01 15:24:39 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+const publicDir = path.join(__dirname, "../public");
+
+app.use(express.static(publicDir));
+
+module.exports = app;
